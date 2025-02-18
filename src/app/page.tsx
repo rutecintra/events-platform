12c6a34af59b3e5639a853b2f815d919a9558c0e
Rute Cintra <ruteocintra@gmail.com>
2025-02-18 13:04:36 -0300

    feat: create components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,29 @@
+import { Button } from "@/components/button";
+import { IconButton } from "@/components/icon-button";
+import { InputRoot, InputIcon, InputField } from "@/components/input";
+import { ArrowRight, Copy, Mail } from "lucide-react";
+
+
 export default function Home() {
   return (
     <main>
-      <div>Hello world!</div>
+
+      <Button type="submit">
+        Confirmar
+        <ArrowRight className="size-6" />
+      </Button>
+
+      <IconButton>
+        <Copy />
+      </IconButton>
+
+      <InputRoot>
+          <InputIcon>
+            <Mail />
+          </InputIcon>
+          <InputField type="text" placeholder="E-mail" />
+        </InputRoot>
+
     </main>
   );
 }
